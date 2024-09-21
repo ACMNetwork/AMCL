e9bfe4e5cc593cc03a23e603307c09221864d03f
Invatoric io <145204041+invatoric@users.noreply.github.com>
2024-09-21 12:15:00 +0800

    Update Metadata.java

--- HMCL/src/main/java/org/jackhuang/hmcl/Metadata.java
+++ HMCL/src/main/java/org/jackhuang/hmcl/Metadata.java
@@ -32,7 +32,7 @@ public final class Metadata {
 
     public static final String NAME = "AMCL";
     public static final String FULL_NAME = "ACM Minecraft Launcher (A fork of HMCL)";
-    public static final String VERSION = "v1.0.0";
+    public static final String VERSION = "1.0.0";
 
     public static final String TITLE = NAME + " " + VERSION;
     public static final String FULL_TITLE = FULL_NAME + " v" + VERSION;
